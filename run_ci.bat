3e1da1d59d1cb9230fb0bc27863108cf0712c82c
Francisco Dias <Dias.FranciscoA@gmail.com>
2024-08-07 10:51:41 +0100

    Added extra params to the bat file

--- run_ci.bat
+++ run_ci.bat
@@ -3,11 +3,18 @@ setlocal
 
 set "CONFIG_FILE=%1"
 set "RSS_FEED=%2"
+set "EXTRA_PARAMS=%3"
 
 set "COMMAND=python framework_launcher.py -cf %CONFIG_FILE%"
 if not "%RSS_FEED%"=="" (
     set "COMMAND=%COMMAND% -f %RSS_FEED%"
 )
 
+if not "%EXTRA_PARAMS%"=="" (
+    set "COMMAND=%COMMAND% %EXTRA_PARAMS%"
+)
+
 echo Running command: %COMMAND%
 %COMMAND%
+
+
