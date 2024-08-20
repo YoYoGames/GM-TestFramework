9e0c34fc1b3873608946fddd706b440b115e094c
Steven Campbell <steven.campbell@yoyogames.com>
2024-08-20 17:15:04 +0100

    Upgrade project format for xUnit Added scripts to upgrade project in future

---
+++ projects/upgrade_project.bat
@@ -0,0 +1,14 @@
+@echo on
+if "%PROJECTTOOL%" == "" set PROJECTTOOL=C:\Source\ProjectTool\Source\bin\Debug\net8.0\ProjectTool
+set BASE=%cd%
+
+%PROJECTTOOL% SHOWVERSIONEDTYPES DESTINATION=resourceslist.json
+
+for /d %%p in (*) do (
+  cd %%p
+  %PROJECTTOOL% project save format=versioned source=.\\%%p.yyp forcevers0=true RESOURCETYPESPATH=%BASE%\\resourceslist.json PREFABSFOLDER=.
+  cd ..
+)
+
+cd %BASE%
+del resourceslist.json
